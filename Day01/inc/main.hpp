78ffed242328d6096412c068b37a8744df7e91c3
Servallou <77835600+Servallou@users.noreply.github.com>
2023-07-17 08:52:41 +0200

    Initial commit

---
+++ Day01/inc/main.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** Day01
+** File description:
+** main
+*/
+
+#ifndef MAIN_HPP_
+    #define MAIN_HPP_
+
+    #include <iostream>
+
+#endif /* !MAIN_HPP_ */
---
+++ Day01/src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** Main
+** File description:
+** main
+*/
+
+#include "main.hpp"
+
+int main(int ac, char **av)
+{
+    uint64_t ID;
+	return 0;
+}
